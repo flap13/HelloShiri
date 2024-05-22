05a4d71d3f6ea8c52f4081f3addfff32abaefd98
flap13 <flap13@gmail.com>
2024-05-22 22:42:35 +0300

    delete old

--- Models/TextValue.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Models;
-
-public class TextValue
-{
-    public string Name { get; set; } = string.Empty;
-    public string Value { get; set; } = string.Empty;
-}
-
--- ShiriAI/IShiriMachine.cs
+++
@@ -1,8 +0,0 @@
-﻿using Models;
-
-namespace ShiriAI;
-
-public interface IShiriMachine
-{
-    ChatResponse GetChatResponse(ChatRequest chatRequest);
-}
